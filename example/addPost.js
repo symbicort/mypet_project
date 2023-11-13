dd1a8e71ec387127515056418c202ecf725451ea
symbicort <jeongwonlee0@gmail.com>
2023-11-14 02:37:33 +0900

    1114 project:commit

---
+++ example/addPost.js
@@ -0,0 +1,54 @@
+function addPost(){
+    const item = JSON.parse(sessionStorage.getItem("isLoginUser"));
+    const title = String(document.querySelector('#title').value); 
+    const content = String(document.querySelector('#content').value);
+
+    if (!item.isLogin) {
+        alert('로그인 후 페이지를 확인해주세요.');    
+        window.location.href = 'index.html';
+    }
+    const request = indexedDB.open("myPetDB", 1);
+
+    // 데이터베이스 열기 성공 시 호출되는 콜백
+    request.onsuccess = function (event) {
+
+    const db = event.target.result;
+
+    // 트랜잭션 시작 (읽기/쓰기 권한 필요)
+    const transaction = db.transaction(['posts'], 'readwrite');
+
+    // 데이터베이스 객체 저장소(테이블) 열기
+    const postsStore = transaction.objectStore('posts');
+
+    const nowDate = new Date();        
+    const timestamp = nowDate.toISOString();
+
+    const pattern = /\s/;
+
+    if(pattern.test(title) || title.length < 3){
+        alert('제목에 공백이 기입되어 있거나 3자 이하인지 확인해주세요.')
+    } else if(pattern.test(content) || content.length < 5){
+        alert('내용에 공백이 기입되어 있거나 5자 이하인지 확인해주세요.')
+    } else {
+        // 추가할 데이터
+        const postData = {
+            author: item.userName,
+            title: title,
+            content: content,
+            timestamp: timestamp,
+        };
+        // 데이터 추가 (add() 메서드 사용)
+        const addRequest = postsStore.add(postData);
+
+        addRequest.onsuccess = function (event) {
+            console.log('데이터가 성공적으로 추가되었습니다.');
+        };
+        addRequest.onerror = function (event) {
+            console.error('데이터 추가 중 오류 발생:', event.target.error);
+        };
+        // 트랜잭션 완료 시
+        transaction.oncomplete = function () {
+            console.log('트랜잭션이 완료되었습니다.');
+        };
+    };
+}};
---
+++ example/permissionChk.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const isLoginUser = JSON.parse(sessionStorage.getItem("isLoginUser"));
+    if (!isLoginUser || !isLoginUser.isLogin) {
+        window.location.href = 'login.html';
+        alert('로그인 후 페이지를 확인해주세요.');
+        
+    }
+});
