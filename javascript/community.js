743cae0644bdfaf2dc44e741a50209db60d77343
k1mhappy <gudwl0115@naver.com>
2023-11-16 12:39:26 +0900

    게시글 상세 숨김

---
+++ javascript/community.js
@@ -0,0 +1,7 @@
+function toggleContent() {
+    let hiddenContent = document.getElementById('content1');
+
+    // 토글: 'hidden' 클래스를 추가 또는 제거하여 숨김 또는 표시 상태를 변경합니다.
+    hiddenContent.classList.toggle('visible');
+
+}
