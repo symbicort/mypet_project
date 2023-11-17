81fbb92b597963dd5158c3c098339cab4eaa78ed
k1mhappy <gudwl0115@naver.com>
2023-11-18 05:27:41 +0900

    자유커뮤니티, 상세커뮤니티 일부 작업

--- javascript/community.js
+++ javascript/community.js
@@ -1,8 +1,3 @@
-function toggleContent() {
-    let hiddenContent = document.getElementById('content1');
-    // 토글: 'hidden' 클래스를 추가 또는 제거하여 숨김 또는 표시 상태를 변경합니다.
-    hiddenContent.classList.toggle('visible');
-}
 
 function writePost() {
     window.location.href = 'write.html'
