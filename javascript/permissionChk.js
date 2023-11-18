e4200fd09e7021afa474bdc0fafd8747e1e66c91
symbicort <jeongwonlee0@gmail.com>
2023-11-18 09:40:22 +0900

    미로그인 시 커뮤니티 페이지 접근 block

--- javascript/permissionChk.js
+++ javascript/permissionChk.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function (){
+function loginChk(){
     const isLoginUser = JSON.parse(sessionStorage.getItem("isLoginUser"));
     if (!isLoginUser || !isLoginUser.isLogin) {
         window.location.href = 'login.html';
         alert('로그인 후 페이지를 확인해주세요.');
-        
     }
-});
+}
